813fd38abdff322f3b4494655f888319b78d5ba2
Sunder Samota <sundersamota001@gmail.com>
2023-03-03 13:55:54 +0530

    update navbar file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -30,29 +30,6 @@ const NavBar = () => {
   const btnRef = React.useRef();
   return (
     <>
-      <Drawer
-        isOpen={isOpen}
-        placement="top"
-        onClose={onClose}
-        finalFocusRef={btnRef}
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>Create your account</DrawerHeader>
-
-          <DrawerBody>
-            <Input placeholder="Type here..." />
-          </DrawerBody>
-
-          <DrawerFooter>
-            <Button variant="outline" mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button colorScheme="blue">Save</Button>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
       <Flex
         boxShadow='lg'
         h="70px"
